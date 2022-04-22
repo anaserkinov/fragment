62b9ae9ded6b917523a082d945af9f0999f9dab4
anasxonerkinjonov <anasxonerkinjonov@gmail.com>
2022-04-22 11:26:54 +0500

    Add working hours in hr

--- lib/src/main/java/com/ailnor/fragment/Fragment.kt
+++ lib/src/main/java/com/ailnor/fragment/Fragment.kt
@@ -38,7 +38,7 @@ abstract class Fragment : LifecycleOwner {
     private var finishing = false
     protected var hasToolbar = true
     var groupId = -1
-    var id = -1
+    var fragmentId = -1
 
     val context: Context
         get() = parentLayout!!.context
